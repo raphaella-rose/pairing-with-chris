f327bf220de21c46dd2c3ff8cf4306f1889770ad
Christopher <christopher2brown@hotmail.co.uk>
2022-05-18 15:08:45 +0100

    completed challenge 4

--- lib/debugging-challenge.rb
+++ lib/debugging-challenge.rb
@@ -1,15 +1,13 @@
 def get_most_common_letter(text)
-  p text
   counter = Hash.new(0)
-  p counter
   text.chars.each do |char|
+    if !char.include? (" ") 
     counter[char] += 1
+    end
   end
-  p counter
-  counter.to_a.sort_by { |k, v| v }
-  p counter.to_a.sort_by { |k, v| v } 
-  [0][0]
-
+  last_index = counter.to_a.length - 1
+  counter.to_a.sort_by { |k, v| v }[last_index][0]
+ 
 end
 
 p get_most_common_letter("the roof, the roof, the roof is on fire!")
